b96dc25ad891811976b0a44e47986a614ed4bd15
rtetelbaum <rtetelbaum@mailbox.org>
2020-09-18 13:50:58 -0400

    roman submitting lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    split_array = self.split(/[!.?]+/)
+    split_array.count
   end
 end
